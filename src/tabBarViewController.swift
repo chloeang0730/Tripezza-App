8bb153f398095feaddeccec0976e9de55b8ef0c0
chloeang0730 <130808941+chloeang0730@users.noreply.github.com>
2024-08-02 13:37:27 +1000

    Upload tabBarViewController.swift

---
+++ src/tabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  tabBarViewController.swift
+//  Tripezza
+//
+//  Created by Chloe Ang on 01/05/2023.
+//
+
+import UIKit
+
+class tabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.hidesBackButton = true 
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
